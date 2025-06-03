e7fe460a6ad48ad2b1195de40f25d9cfdee4f4b4
Yudai Fujimoto <apricot4096@gmail.com>
2025-06-04 00:51:35 +0900

    perf: 1D blocking

--- src/cuda/gemm.cu
+++ src/cuda/gemm.cu
@@ -13,58 +13,54 @@ using namespace std;
     #define GEMM_KERNEL_NAME sgemm_kernel
 #endif
 
-#define TS_M 32
-#define TS_N 32
-#define TS_L 32
+#define BN 64
+#define BK 8
+#define TM 8
 
 namespace cuda {
 
     __global__ void GEMM_KERNEL_NAME(data_t *A, data_t *B, data_t *C, size_t N) {
 
-        int M = N;
-        // int L = N;
+        uint thr_i = threadIdx.y;
+        uint thr_j = threadIdx.x;
+        uint blk_i = blockIdx.y;
+        uint blk_j = blockIdx.x;
 
-        int i = blockIdx.y * blockDim.y + threadIdx.y;
-        int j = blockIdx.x * blockDim.x + threadIdx.x;
+        data_t c[TM] = {0.0};
+        __shared__ data_t _A[BN][BK];
+        __shared__ data_t _B[BK][BN];
 
-        if (i >= M || j >= N) return;
-
-        __shared__ data_t _A[TS_M][TS_L];
-        __shared__ data_t _B[TS_L][TS_N];
-        
-        data_t c = 0.0;
-
-        i = threadIdx.y;
-        j = threadIdx.x;
-
-        int I = blockIdx.y * TS_M;
-        int J = blockIdx.x * TS_N;
-
-        for (int K = 0; K < N; K+=TS_L) {
+        for (int K = 0; K < N; K+=BK) {
             
-            // load into shared memory        
-            _A[i][j] = A[(I+i)*N + (K+j)];
-            _B[i][j] = B[(K+i)*N + (J+j)];
+            // load into shared memory
+            _A[thr_j][thr_i] = A[(BN*blk_i+thr_j) *N+ (K+thr_i)];
+            _B[thr_i][thr_j] = B[(K+thr_i) *N+ (blk_j*BN+thr_j)];
 
             __syncthreads();
 
-            // compute sub matrix
-            for (int k = 0; k < TS_L; k++) {
-                c += _A[i][k] * _B[k][j];
+            // compute
+            for (int i = 0; i < BK; i++) {
+                data_t scalar = _B[i][thr_j];
+                for (int k = 0; k < TM; k++) {
+                    c[k] += _A[thr_i*TM+k][i] * scalar;
+                }
             }
-
+            
             __syncthreads();
         }
-
-        C[(I+i) * N + (J+j)] = c;
+        
+        // write back
+        for (int k = 0; k < TM; k++) {
+            C[(blk_i*BN+thr_i*TM+k) *N+ (blk_j*BN+thr_j)] = c[k];
+        }
 
     }
     
 
     void GEMM_NAME(data_t *A, data_t *B, data_t *C, size_t N) {
-        dim3 ThreadsPerBlocks(TS_M, TS_N);
-        dim3 BlocksPerGrids((N + TS_M - 1) / TS_M,
-                            (N + TS_N - 1) / TS_N);
+        dim3 ThreadsPerBlocks(BN, BK);
+        dim3 BlocksPerGrids((N + BN - 1) / BN,
+                            (N + BK - 1) / BK / TM);
         GEMM_KERNEL_NAME<<<BlocksPerGrids, ThreadsPerBlocks>>>(A, B, C, N);
     } 
 
